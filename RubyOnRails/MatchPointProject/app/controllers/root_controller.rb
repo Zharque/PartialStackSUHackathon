b426066c3a842a086c5969c7b7c8d24c43f8dc4b
Mint Cola <ryndeeplayer@gmail.com>
2024-03-02 11:54:10 +0800

    v1

--- RubyOnRails/MatchPointProject/app/controllers/root_controller.rb
+++ RubyOnRails/MatchPointProject/app/controllers/root_controller.rb
@@ -1,2 +1,15 @@
 class RootController < ApplicationController
+  def signup
+    @account = UserAccount.new
+    @account.name = params[:name]
+    @account.email = params[:email]
+    @account.password = params[:password]
+    # @account.birthdate = params[:birthdate]
+    @account.address = params[:address]
+    @account.skills = params[:skills]
+    @account.expertise = params[:expertise] 
+    @account.user_type = params[:user_type]
+   
+    @account.save
+  end
 end
